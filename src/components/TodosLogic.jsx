951d2ad0dd36a237d3d3a47cbf0e0fe7ce2ddd5b
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-10-24 15:43:00 +0100

    Add logic in TodosLogic to loop through todos

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,7 +1,34 @@
 
 const TodosLogic = () => {
+    // Mock List
+    const todos = [
+        {
+            id: 1,
+            title: 'Setup development environment',
+            completed: true,
+        },
+        {
+            id: 2,
+            title: 'Develop website and add content',
+            completed: false,
+        },
+        {
+            id: 3,
+            title: 'Deploy to live server',
+            completed: false,
+        },
+    ];
+
     return (
-        <div>TodosLogic Content</div>
+        /* To Display the list we need to loop through the array using the js map() function and return the title of the individual todo*/
+        <>
+            <ul>
+                {todos.map(todo => (
+                    <li>{todo.title}</li>
+                ))}
+            </ul>
+
+        </>
     );
 };
 
