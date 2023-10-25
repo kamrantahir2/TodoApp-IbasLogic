a08e8d56be1f83b1b5bcfec2dadce0a59c02aca3
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-10-25 01:41:12 +0100

    Add checkbox in TodoItem

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -2,7 +2,14 @@
 // This component will render the actual li item to be rendered in TodosList
 
 const TodoItem = ({ itemProp }) => {
-    return <li>{itemProp.title}</li>;
+    return (
+        <>
+            <li>
+                <input type="checkbox" />
+                {itemProp.title}
+            </li>
+        </>
+    );
 };
 
 export default TodoItem;
