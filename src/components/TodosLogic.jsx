737a23755cd5e9de50c0a262fe4017291cfd0f0c
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-10-25 02:58:42 +0100

    Install uuid to project and replace hardcoded id's with uuid

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,22 +1,23 @@
 import InputTodo from "@/components/InputTodo";
 import TodosList from "@/components/TodosList";
 import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 const TodosLogic = () => {
     // Mock List
     const [todos, setTodos] = useState([
         {
-            id: 1,
+            id: uuidv4(),
             title: 'Setup development environment',
             completed: true,
         },
         {
-            id: 2,
+            id: uuidv4(),
             title: 'Develop website and add content',
             completed: false,
         },
         {
-            id: 3,
+            id: uuidv4(),
             title: 'Deploy to live server',
             completed: false,
         },
@@ -44,7 +45,7 @@ const TodosLogic = () => {
 
     const addTodoItem = (title) => {
         const newTodo = {
-            id: 4,
+            id: uuidv4(),
             title: title,
             completed: false
         };
