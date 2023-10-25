d6b6523856748b99250674118f47d8a7e46a2c2c
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-10-25 02:54:19 +0100

    create addTodoItem function and drill inot InputTodo from TodosLogic

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,6 +1,25 @@
+import { useState } from 'react';
 
-const InputTodo = () => {
-    return <div>input field here...</div>;
+
+const InputTodo = ({ addTodoItem }) => {
+    const [title, setTitle] = useState('');
+
+    const handleChange = (e) => {
+        setTitle(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(title);
+        addTodoItem(title);
+        setTitle('');
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Add Todo..." value={title} onChange={handleChange} />
+        </form>
+    );
 };
 
 export default InputTodo;
