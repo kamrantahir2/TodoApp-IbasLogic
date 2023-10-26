2288e5e22c7a922e544f9cd6ce2f1f42316a0193
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-10-26 23:02:43 +0100

    Create TodosContext file, import createContext and create a TodosProvider function which gives all its children access to the data

---
+++ context/TodosContext.jsx
@@ -0,0 +1,21 @@
+
+// Global data will be kept in a context store and will expose them for other components to consume
+
+import { createContext } from "react";
+
+const TodosContext = createContext(null);
+
+// Any descendants of this provider will have access to the context data
+export const TodosProvider = ({ children }) => {
+    return (
+
+        // All components that will be descendents of <TodosProvider> will have access to whatever we assign to the provider's value prop
+        <TodosContext.Provider>
+            {children}
+        </TodosContext.Provider>
+    );
+};
+
+export { TodosContext };
+
+// We will now be heading to TodosLogic to wrap the entire jsx in <TodosProvider>. This will allow the components to have access to the context
