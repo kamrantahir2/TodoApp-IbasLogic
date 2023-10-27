c206d2518059841322d80b7cec0394936b7a3d3a
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-10-28 00:58:57 +0100

    Connect main.jsx to Router

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,18 @@ import ReactDOM from 'react-dom/client';
 import TodoApp from '@/components/TodoApp';
 import '@/styles/app.css';
 
+// Import React Router
+import { BrowserRouter } from 'react-router-dom';
+
 const domContainer = document.getElementById('root');
 
 const root = ReactDOM.createRoot(domContainer);
 
+// We wrap the top level component in a BrowserRouter component
 root.render((
     <React.StrictMode>
-        <TodoApp />
+        <BrowserRouter>
+            <TodoApp />
+        </BrowserRouter>
     </React.StrictMode>
 ));
