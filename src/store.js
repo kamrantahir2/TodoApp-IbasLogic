cd7c8ed25addeb8e7e94ea1956452f37ca1d8343
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-10-27 13:24:06 +0100

    Add code to todosStore function in store.js to handle adding new todo to the list

--- src/store.js
+++ src/store.js
@@ -1,9 +1,24 @@
 
 // We will be using Zustand instead of Redux. The difference between this and a context provider is that the components don't need to be wrapped in the context provider
 import { create } from "zustand";
+import { v4 as uuidv4 } from 'uuid';
 
-const todosStore = (set) => {
+const todosStore = (set) => ({
+    // Create new array so we don't alter the existing stat
+    todos: [],
+    // Create a new Todo when for when the user adds one
+    addTodoItem: (title) => {
+        const newTodo = {
+            id: uuidv4(),
+            title, title,
+            completed: false,
+        };
+            // Set the state to include the new Todo
+        set((state) => ({
+            todos: [...state.todos, newTodo]
+        }));
+    }
 
-};
+});
 
 export const useTodosStore = create(todosStore);
