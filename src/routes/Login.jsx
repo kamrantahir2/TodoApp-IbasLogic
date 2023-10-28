84ef2ad6601fd80558b9a7608cc787b1405acc71
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-10-28 22:14:19 +0100

    Build Login component consisting of input field and button. Add username state for keeping track fo username

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,6 +1,35 @@
 
+//  We will only check if a user exists instead of athenticating by password
+
+import { useState } from "react";
+import styles from '@/styles/Login.module.css';
+
+// We will create a form to take the user's input
 const Login = () => {
-    return <div>Login page</div>;
+
+    const [username, setUsername] = useState('');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!username) return;
+        console.log(username);
+    };
+
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <div className={styles.formWrapper}>
+                <form className={styles.form} onSubmit={handleSubmit} >
+                    <input type="text"
+                        placeholder="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <button>Login</button>
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default Login;
