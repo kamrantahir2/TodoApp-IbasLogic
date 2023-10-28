bb67e5954d2c8ac6ddf8995e8b51023542763fa0
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-10-28 21:45:36 +0100

    Fill in About component whos links will no render SinglePage as defined in TodoApp

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,6 +1,25 @@
 
+// Whenever we have a children route we will use Outlet in the parent route (in this, case About) to render the children (in this case, the single page content)
+import { NavLink, Outlet } from "react-router-dom";
+
 const About = () => {
-    return <div>About Content</div>;
+    return (
+        <>
+            <h1>About Page</h1>
+            <div className="about">
+                <ul className="about_list">
+                    <li>
+                        <NavLink to='about-app' >About App</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='about-developer' >About developer</NavLink>
+                    </li>
+                </ul>
+                <Outlet />
+            </div>
+        </>
+
+    );
 };
 
 export default About;
