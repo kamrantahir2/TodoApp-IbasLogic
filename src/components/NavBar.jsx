d143d129d73f9700e5b355623150a4f7cd9bf8ab
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-10-29 01:03:14 +0000

    Add logout button when user is logged in

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,9 @@
 
 import { NavLink } from "react-router-dom";
 
+// We have already created the logout function, all we have to do is incorporate it:
+import { useAuthContext } from "@/context/AuthContext";
+
 // This file will be used as a shared layout across all routes
 
 const links = [
@@ -11,19 +14,36 @@ const links = [
 ];
 
 const NavBar = () => {
+
+    // Grab the current state and the logout function from AuthContext
+    const { user, logout } = useAuthContext();
+    const handleLogout = () => {
+        logout();
+    };
+
+    // We will only display the logout feature if the user is logged in. We do this by calling the current state from above and using &&
     return (
-        <nav className="navbar">
-            <ul>
-                {links.map((link) => {
-                    return (
-                        <li key={link.text} >
-                            <NavLink to={link.path}
-                            >{link.text}</NavLink>
-                        </li>
-                    );
-                })}
-            </ul>
-        </nav>
+        <>
+            <nav className="navbar">
+                <ul>
+                    {links.map((link) => {
+                        return (
+                            <li key={link.text} >
+                                <NavLink to={link.path}
+                                >{link.text}</NavLink>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </nav>
+            {user && (
+                <div className="logout">
+                    <p>{user}</p>
+                    {<button onClick={handleLogout}>Logout</button>}
+                </div>
+            )}
+        </>
+
     );
 };
 
