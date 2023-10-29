074c792e45bcd6c3a845e56c2dcc56bfda116cf7
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-10-29 02:53:17 +0000

    Persist user state in browser storage

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,16 +1,33 @@
 
 // We will take the data from the Login component and pass it to a tontext store that lets us broadcast it
 
-import { useState, useContext, createContext } from "react";
+import { useState, useContext, createContext, useEffect } from "react";
 
+// We created a context store that helps to manage the user's input and the login and logout functionalities.
 
 const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
 
-    // We created a context store that helps to manage the user's input and the login and logout functionalities. 
 
-    const [user, setUser] = useState('');
+
+    // Just like we added browser storage for our todos for data persistence in TodosContext, we will do the same with the user state.
+    const getUsername = () => {
+        // Getting stored state
+        const temp = localStorage.getItem('username');
+        const savedUsername = JSON.parse(temp);
+        return savedUsername || '';
+    };
+
+    const [user, setUser] = useState(getUsername());
+
+
+
+    useEffect(() => {
+        // Storing user state
+        const temp = JSON.stringify(user);
+        localStorage.setItem('username', temp);
+    }, [user]);
 
     //  We created a login function that expects the user's input from the login form to update the global user state. This function will be used in the Login component to take the current username
     const login = (user) => setUser(user);
