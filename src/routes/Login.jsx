ceba62f8fb0c64dbda87cdb69d4463d8170d3b59
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-10-29 01:43:48 +0000

    Navigate away from login page after successful login

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -5,6 +5,9 @@ import { useState } from "react";
 import styles from '@/styles/Login.module.css';
 import { useAuthContext } from "@/context/AuthContext";
 
+// We want to navigate away from the login screen to an intended route after a successful login. To do this we can take advantage of the useNavigate Hook from React Router
+import { useNavigate } from "react-router-dom";
+
 // We will create a form to take the user's input
 const Login = () => {
 
@@ -12,12 +15,17 @@ const Login = () => {
     // The current state is now available 
     const { login } = useAuthContext();
 
+    // We will use useNavigate to navigate away from the login screen after a successful login, this will be done in handleSubmit:
+    const navigate = useNavigate();
+
     const [username, setUsername] = useState('');
     const handleSubmit = (e) => {
         e.preventDefault();
         if (!username) return;
         login(username);
         console.log(username);
+        // The replace option means that we won't go back to the login screen when we press the back button after a successful login
+        navigate('/', { replace: true });
     };
 
 
